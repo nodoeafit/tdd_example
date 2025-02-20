c85b6e64e29a8f9398ab8b6353feb1271af5ad46
diegolsg <diegos85137@gmail.com>
2025-02-19 22:58:03 -0500

    new test v-d

--- OrderService.Test/OrderServiceTest.cs
+++ OrderService.Test/OrderServiceTest.cs
@@ -2,19 +2,54 @@ using Xunit;
 using Moq;
 using OrderService;
 
-public class OrderServiceTest 
+public class OrderServiceTest
 {
     [Fact]
-    public void CreateOrder_ShouldThrowException_WhenOrderIsNull(){
+    public void CreateOrder_ShouldThrowException_WhenOrderIsNull()
+    {
         var service = new OrderService.OrderService();
-        Assert.Throws<ArgumentNullException>(()=> service.CreateOrder(null));
+        Assert.Throws<ArgumentNullException>(() => service.CreateOrder(null));
     }
 
     [Fact]
-    public void CreateOrder_ShouldReturnOrderId(){
+    public void CreateOrder_ShouldReturnOrderId()
+    {
         var service = new OrderService.OrderService();
-        var order = new Order{ Id = 1, Amount = 20000 };
+        var order = new Order { Id = 1, Amount = 20000 };
         var orderId = service.CreateOrder(order);
         Assert.Equal(order.Id, orderId);
+    }
+
+    [Fact]
+    public void FindOrderById_ShouldReturnOrder_WhenOrderExists()
+    {
+        var service = new OrderService.OrderService();
+        var foundOrder = service.FindOrderById(1);
+        Assert.NotNull(foundOrder);
+    }
+
+    [Fact]
+    public void FindOrderById_ShouldReturnNull_WhenOrderDoesNotExist()
+    {
+        var service = new OrderService.OrderService();
+        var foundOrder = service.FindOrderById(1);
+        Assert.Equal(1, foundOrder.Id);
+
+    }
+
+    [Fact]
+    public void FindAmountGreaterThan_ShouldReturnOrders_WhenAmountIsGreaterThan()
+    {
+        var service = new OrderService.OrderService();
+        var orders = service.FindAmountGreaterThan(1000);
+        Assert.Contains(orders, o => o.Amount > 1000);
+    }
+
+    [Fact]
+    public void FindAmountGreaterThan_ShouldThrowException_WhenAmountIsNull()
+    {
+        var service = new OrderService.OrderService();
+        Assert.Throws<KeyNotFoundException>(() => service.FindAmountGreaterThan(100000000));
+
     }
 }
--- OrderService/OrderService.cs
+++ OrderService/OrderService.cs
@@ -1,11 +1,49 @@
 namespace OrderService {
-    public class OrderService {
-        public int CreateOrder (Order? order){
-          if(order == null){
-            throw new ArgumentNullException();
-          }
-          
-          return order.Id;   
+    public class OrderService
+    {
+        private List<Order> orders;
+
+        public OrderService()
+        {
+            orders = new List<Order>
+            {
+                new Order { Id = 1, Amount = 20000 },
+                new Order { Id = 2, Amount = 10000 },
+                new Order { Id = 3, Amount = 50000 }
+            };
+        }
+
+        public int CreateOrder(Order? order)
+        {
+            if (order == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return order.Id;
+        }
+
+        public Order? FindOrderById(int id)
+        {
+
+            var order = orders.Find(o => o.Id == id);
+            if (order == null)
+            {
+                throw new KeyNotFoundException("No se encontró ninguna orden con ese ID.");
+            }
+
+            return order;
+        }
+
+        public List<Order> FindAmountGreaterThan(decimal amount)
+        {
+            var order = orders.Where(o => o.Amount > amount).ToList();
+            if (order == null || !order.Any())
+            {
+                throw new KeyNotFoundException();
+            }
+            return order;
         }
     }
+
 }
