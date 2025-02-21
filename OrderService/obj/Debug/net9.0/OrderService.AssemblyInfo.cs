6a5d149f1a1c5bb381d5572df9bd74877add0755
SEBASTIAN DAVILA GOMEZ <103950566+SDagoG19@users.noreply.github.com>
2025-02-21 18:14:13 -0500

    restores

--- OrderService/obj/Debug/net9.0/OrderService.AssemblyInfo.cs
+++ OrderService/obj/Debug/net9.0/OrderService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c35b762502fa7d04fa0d53dbe3d0266bf6b9843e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b06ea14f51b056f6c41b86aeac65c756bb64b70f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
