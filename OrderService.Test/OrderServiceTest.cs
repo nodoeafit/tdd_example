7f7696a074aa6d40bd1625b182dfed69b0e8f218
Vero-nica22 <mcveronica66@gmail.com>
2025-02-21 18:10:16 -0500

    Pruebas

--- OrderService.Test/OrderServiceTest.cs
+++ OrderService.Test/OrderServiceTest.cs
@@ -2,36 +2,48 @@ using Xunit;
 using Moq;
 using OrderService;
 
-public class OrderServiceTest 
+public class OrderServiceTest
 {
     [Fact]
-    public void CreateOrder_ShouldThrowException_WhenOrderIsNull(){
+    public void CreateOrder_ShouldThrowException_WhenOrderIsNull()
+    {
         var service = new OrderService.OrderService();
-        Assert.Throws<ArgumentNullException>(()=> service.CreateOrder(null));
+        Assert.Throws<ArgumentNullException>(() => service.CreateOrder(null));
     }
 
     [Fact]
-    public void CreateOrder_ShouldReturnOrderId(){
+    public void CreateOrder_ShouldReturnOrderId()
+    {
         var service = new OrderService.OrderService();
-        var order = new Order{ Id = 1, Amount = 20000 };
+        var order = new Order { Id = 1, Amount = 20000 };
         var orderId = service.CreateOrder(order);
         Assert.Equal(order.Id, orderId);
     }
 
     [Fact]
-    public void CreateOrder_ShouldThrowException_WhenOrderAmountIsZero(){
+    public void CreateOrder_ShouldThrowException_WhenOrderAmountIsZero()
+    {
         var service = new OrderService.OrderService();
         var order = new Order { Id = 1, Amount = 0 };
         Assert.Throws<ArgumentException>(() => service.CreateOrder(order));
     }
     [Fact]
-    public void CreateOrder_ShouldApplyPromotion_WhenOrderHasPromotion(){
+    public void CreateOrder_ShouldApplyPromotion_WhenOrderHasPromotion()
+    {
         var service = new OrderService.OrderService();
         var order = new Order { Id = 3, Amount = 10000, HasPromotion = true };
         var orderId = service.CreateOrder(order);
         Assert.Equal(order.Id, orderId);
         Assert.Equal(9000, order.Amount);
     }
-    
-    
+
+    [Fact]
+    public void CreateOrder_ShouldThrowException_WhenOrderAmountIsNegative()
+    {
+        var service = new OrderService.OrderService();
+        var order = new Order { Id = 1, Amount = -1 };
+        Assert.Throws<ArgumentException>(() => service.CreateOrder(order));
+    }
+
+
 }
