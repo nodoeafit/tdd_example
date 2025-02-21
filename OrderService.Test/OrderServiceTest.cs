0a4aab922949e6eb460c5c331fb5cee21bbfb2f1
Maria <julianahenao1510@gmail.com>
2025-02-21 10:52:10 -0500

    Separé las lineas del codigo

--- OrderService.Test/OrderServiceTest.cs
+++ OrderService.Test/OrderServiceTest.cs
@@ -4,30 +4,43 @@ public class OrderServiceTest
 {
     [Fact]
     public void CreateOrder_ShouldThrowException_WhenOrderIsNull(){
+
         var service = new OrderService.Services.OrderService();
+
         Assert.Throws<ArgumentNullException>(()=> service.CreateOrder(null));
     }
 
     [Fact]
     public void CreateOrder_ShouldReturnOrderId(){
+
         var service = new OrderService.Services.OrderService();
+
         var order = new Order{ Id = 1, Amount = 20000 };
+
         var orderId = service.CreateOrder(order);
+
         Assert.Equal(order.Id, orderId);
     }
 
     [Fact]
     public void GetOrderById_ShouldThrowException_WhenOrderDoesNotExist(){
+
         var service = new OrderService.Services.OrderService();
+
         Assert.Throws<KeyNotFoundException>(()=> service.GetOrderById(4));
     }
 
     [Fact]
     public void GetOrderById_ShouldReturnOrder_WhenOrderIdExists(){
+
         var service = new OrderService.Services.OrderService();
+
         var orderExist = new Order{ Id = 1, Amount = 20000 };
+
         service.CreateOrder(orderExist);
+
         var result = service.GetOrderById(orderExist.Id);
+
         Assert.Equal(orderExist.Id, result.Id);
         Assert.Equal(orderExist.Amount, result.Amount);
     }
