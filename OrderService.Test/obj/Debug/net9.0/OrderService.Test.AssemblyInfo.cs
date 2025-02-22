7eadfa330839c1ad7b86eabfbb9806903f8d31a0
TakUHG <sofiavasqueztabares@gmail.com>
2025-02-21 19:52:46 -0500

    upload

--- OrderService.Test/obj/Debug/net9.0/OrderService.Test.AssemblyInfo.cs
+++ OrderService.Test/obj/Debug/net9.0/OrderService.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b177952375e8590daefe96aabf75c71303ab7d04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e5150d7bfa24276331a8c4165e5a34943e991cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
